672deaa5fb6c22a648504f4932384d7ade54e05e
marvelishere2 <105986148+marvelishere2@users.noreply.github.com>
2023-05-30 21:28:12 +0330

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Not Found</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Oops!</h1>
+		<p>The page you requested could not be found.</p>
+		<a href="/">Go back to homepage</a>
+	</div>
+</body>
+</html>
